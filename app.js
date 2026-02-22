a84f3539cb5749905e516abe35516b8197c2f273
Dishani Hiripitiya <IT23685598@my.sliit.lk>
2026-02-22 11:37:56 +0530

    New node.js Setup

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 console.log('Hello to Node.js!');
 
-const fs = require('fs');
+/*const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
-});
+});*/
+
+
 
 /*const http = require('http');
 
